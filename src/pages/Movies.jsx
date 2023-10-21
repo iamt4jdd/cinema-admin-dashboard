622a63ef047cb809d80c8a46447127bd44429445
LongVu2190 <hoanglongvu912@gmail.com>
2023-10-21 15:12:33 +0700

    update

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -11,7 +11,7 @@ function Modal() {
     return (
         <div>
             <button onClick={toggleModal} type="button" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Add movie
+                Add Movie
             </button>
 
             {isOpen && (
